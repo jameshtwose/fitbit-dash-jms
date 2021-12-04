9b6cea2653c50490acf532f3b6f23189e46d30c0
James Twose <jamestwose@Mac-mini.home>
2021-12-04 13:59:36 +0100

    attempting to fix the cherrpy-heroku issue v3

--- reading_utils.py
+++ reading_utils.py
@@ -11,9 +11,9 @@ import os
 # Load your credentials
 # load_dotenv(find_dotenv())
 
-# import cherrypy
-# cherrypy.config.update({'server.socket_host': '0.0.0.0',})
-# cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
+import cherrypy
+cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
